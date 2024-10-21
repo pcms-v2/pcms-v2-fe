0f222af84b750a4a7b951fe3ce820491389e6baa
minjunpark <mj9457@naver.com>
2024-10-21 15:48:48 +0900

    fix. 배송 요청 상태 버튼 조건 수정

--- src/pages/admin/AdminDeliveryRound/index.jsx
+++ src/pages/admin/AdminDeliveryRound/index.jsx
@@ -248,7 +248,7 @@ const AdminDeliveryRound = () => {
             <Icon iconType='detail' onClick={() => showDeliveryDetail(data)} />
           ),
           deliveryRoundStatus:
-            data.deliveryRoundStatus === 'DELIVERY_ROUND_CLOSE' ? (
+            data.deliveryRoundStatus === 'DELIVERY_REQUEST_CLOSE' ? (
               <CommonButton
                 type='disabled'
                 label={getStatusText(data.deliveryRoundStatus)}
